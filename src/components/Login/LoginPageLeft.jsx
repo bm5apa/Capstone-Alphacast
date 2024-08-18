a329461705456aaff2af80d468d0051f988fd08f
bm5apa <timelessheaven@gmail.com>
2024-08-18 23:18:49 +0800

    build: layout of login-pages

--- src/components/Login/LoginPageLeft.jsx
+++ src/components/Login/LoginPageLeft.jsx
@@ -1,15 +1,27 @@
 import React from 'react';
 import styled from 'styled-components';
+import alphacast_logo from '../../styles/svg/alphacast_logo.png';
+import alphacast_logo_connecting from '../../styles/svg/alphacast_logo_connecting.svg';
+import '../../styles/LoginPageLeft.css';
 
 const LoginPageLeftStyledContainer = styled.div`
-  border: 2px solid blue;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
   width: 100%;
 `;
 
 function LoginPageLeft({}) {
   return (
   <LoginPageLeftStyledContainer>
-  <h1>LoginPageLeft</h1>
+  <img className="alphacast-logo" src={alphacast_logo} />
+  <img className="alphacast-logo_connecting" src={alphacast_logo_connecting} />
+  <a className='login-button' href="https://example.com">使用SPOTIFY帳號登入</a>
+  <div>
+    <span className='login-question'>沒有帳號嗎？</span>
+    <a className='login-signup' href="https://example.com"><span>註冊帳號</span></a>
+  </div>
   </LoginPageLeftStyledContainer>
   );
 }
--- src/components/Login/LoginPageRight.jsx
+++ src/components/Login/LoginPageRight.jsx
@@ -1,15 +1,36 @@
 import React from 'react';
 import styled from 'styled-components';
+import loginphoto1 from '../../styles/svg/loginphoto1.svg';
+import loginphoto2 from '../../styles/svg/loginphoto2.svg';
+import loginphoto3 from '../../styles/svg/loginphoto3.svg';
+import keyboard_arrow_left from '../../styles/svg/keyboard_arrow_left.png';
+import keyboard_arrow_right from '../../styles/svg/keyboard_arrow_right.png';
+import '../../styles/LoginPageRight.css';
 
 const LoginPageRightStyledContainer = styled.div`
-  border: 2px solid green;
+  background-color: rgba(35, 38, 47, 1);
   width: 100%;
 `;
 
 function LoginPageRight({}) {
   return (
   <LoginPageRightStyledContainer>
-  <h1>LoginPageRight</h1>
+    <div className='loginpage-photo-section'>
+      <img className='loginpage-photo' src={loginphoto1} alt="" />
+    </div>
+    <div className="loginpage-arrow-section">
+      <img className='loginpage-arrow-left arrow' src={keyboard_arrow_left} alt="" />
+      <img className='loginpage-arrow-right arrow' src={keyboard_arrow_right} alt="" />
+      </div>
+    <div className='loginpage-description'>
+      <div className="loginpage-title">鼓舞人心的故事</div>
+      <div className="loginpage-subtitle">從非凡的人生故事和成功經歷中獲得靈感</div>
+    </div>
+    <div className='loginpage-pageline-section'>
+      <div className="pageline pageline-1 pageline-white"></div>
+      <div className="pageline pageline-2"></div>
+      <div className="pageline pageline-3"></div>
+    </div>
   </LoginPageRightStyledContainer>
   );
 }
